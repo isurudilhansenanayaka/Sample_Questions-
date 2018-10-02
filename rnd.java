4bcc6ac8d86abef74fe0a7a6e2074fdbd10d641c
isurudilhansenanayaka <iddmdilhan@gmail.com>
2018-10-02 09:17:03 +0530

    select random numbers

---
+++ rnd.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class rnd{
+private static rnd random=new Random();
+public static void main(String args[]){
+    List<String> list=new ArraysList<>();
+list.add("1");
+list.add("2");
+list.add("3");
+list.add("4");
+list.add("5");
+list.add("6");
+
+    getRandomItem(list);
+}
+public static void getRandomItem(List.<String> list){
+
+System.out.println(list.get(index));
+}}
